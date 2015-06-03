8eb3ff72223ef49741410413b0a18521f6a870f2
honghuizhen <honghuizhen@honghuizhen-virtual-machine>
2015-06-03 16:50:45 +0800

    C-cpp

---
+++ my_stack.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include "include/my_stack.h"
+void stack_push(int x)
+{
+int e=1;
+}
+int stack_pop()
+{
+return(1);
+}
+int stack_is_empty()
+{
+return(1); 
+}
+int stack_is_full()
+{
+return 1;
+}
